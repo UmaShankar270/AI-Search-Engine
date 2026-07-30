c058165fe421ae332aae08919ba8b527016d23fd
Vaidhika1105 <matetivaidhika1105@gmail.com>
2026-07-30 19:50:49 +0530

    Added trending repositories API

---
+++ backend/app/routers/repo.py
@@ -0,0 +1,34 @@
+from fastapi import APIRouter
+import requests
+
+router = APIRouter()
+
+@router.get("/trending")
+def trending():
+    url = "https://api.github.com/search/repositories"
+
+    response = requests.get(
+        url,
+        params={
+            "q": "stars:>10000",
+            "sort": "stars",
+            "order": "desc",
+            "per_page": 10
+        }
+    )
+
+    data = response.json()
+
+    repos = []
+
+    for repo in data["items"]:
+        repos.append({
+            "name": repo["name"],
+            "full_name": repo["full_name"],
+            "description": repo["description"],
+            "stars": repo["stargazers_count"],
+            "language": repo["language"],
+            "url": repo["html_url"]
+        })
+
+    return repos
